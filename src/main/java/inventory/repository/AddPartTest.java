18282e56ea00ac48b5d0e460dd7be17c635710fc
andreimateas <mateasandrei10@gmail.com>
2022-03-19 16:31:30 +0200

    Update AddPartTest.java
    test part

--- src/main/java/inventory/repository/AddPartTest.java
+++ src/main/java/inventory/repository/AddPartTest.java
@@ -1,15 +1,24 @@
 package inventory.repository;
 
+import inventory.model.InhousePart;
+import inventory.model.Part;
+import inventory.model.Product;
+import javafx.collections.ObservableList;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddPartTest {
 
+    private Part part;
+    private InventoryRepository repository;
+
     @BeforeEach
     void setUp() {
+        repository= new InventoryRepository();
     }
 
     @AfterEach
@@ -18,5 +27,31 @@ class AddPartTest {
 
     @Test
     void addPart() {
+    }
+
+    @org.junit.jupiter.api.Test
+    @Order(1)
+    void addPart1() {
+        part=new InhousePart(1,"asd",9.0,2,1,4,1);
+        repository.addPart(part);
+    }
+    @org.junit.jupiter.api.Test
+    @Order(2)
+    void addPart2() {
+        part=new InhousePart(1,"fis",-10,2,10,4,1);
+        repository.addPart(part);
+
+    }
+    @org.junit.jupiter.api.Test
+    @Order(3)
+    void addPart3() {
+        part=new InhousePart(1,"crt",11.0,2,10,4,1);
+        repository.addPart(part);
+    }
+    @org.junit.jupiter.api.Test
+    @Order(4)
+    void addPart4() {
+        part=new InhousePart(1,"ndf",11.0,2,1,4,1);
+        repository.addPart(part);
     }
 }
