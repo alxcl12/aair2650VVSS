0a25056f44dab4eadfd740abd95efd02cb39bfa7
andreimariei <andrei.mariei05@gmail.com>
2022-04-02 12:29:11 +0300

    Teste

---
+++ src/main/java/inventory/repository/SerachProductTest.java
@@ -0,0 +1,35 @@
+package inventory.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SerachProductTest {
+
+    private InventoryRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository= new InventoryRepository();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    @Order(1)
+    void lookupProduct1() {
+        String nume="product2";
+        assert(repository.lookupProduct(nume).getName()!=null);
+    }
+    @org.junit.jupiter.api.Test
+    @Order(2)
+    void lookupProduct2() {
+        String nume="asdfffghf";
+        assert(repository.lookupProduct(nume)==null);
+    }
+}
